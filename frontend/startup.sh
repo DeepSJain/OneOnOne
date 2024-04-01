f35f5d454d357dcc99efa000934a11098cce9e81
Deep Jain <deepsjain123@gmail.com>
2024-04-01 06:38:33 -0400

    fix frontend bash files

--- frontend/startup.sh
+++ frontend/startup.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
+source ~/.nvm/nvm.sh
 nvm install 20.11.1
 nvm use 20.11.1
 
