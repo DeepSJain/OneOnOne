e6dffd1a6119d278c8f5dfd91d1673781ea3c515
Deep Jain <deepsjain123@gmail.com>
2024-04-01 04:43:02 -0400

    regenerate package-lock.json and nodejs version

--- frontend/startup.sh
+++ frontend/startup.sh
@@ -1,7 +1,11 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt install nodejs -y
-sudo apt install npm -y
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install 20.11.1
+nvm use 20.11.1
+
+sudo apt-get install npm -y
 
 npm install
